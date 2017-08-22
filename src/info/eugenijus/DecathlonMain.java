b27f0aa2003c1c3db5d5374793b09ceaec0d465d
Eugenijus Sabaliauskas <e.sabaliauskas@gmail.com>
2017-08-22 17:14:50 +0300

    changed Main to DecathlonMain

--- src/info/eugenijus/DecathlonMain.java
+++ src/info/eugenijus/DecathlonMain.java
@@ -5,11 +5,11 @@ package info.eugenijus;
  * @author Eugenijus Sabaliauskas
  *
  */
-public class Main {
+public class DecathlonMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
